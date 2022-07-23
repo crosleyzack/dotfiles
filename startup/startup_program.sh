4d11469aa5dde65fe61700b9d169ea40d2ecdb68
Zack Crosley <zcrosley@censys.io>
2022-07-23 08:05:12 -0400

    Shell script now loops through windows and can increment virtual desktop

--- startup/startup_program.sh
+++ startup/startup_program.sh
@@ -10,33 +10,52 @@ function launch {
 	#	- 1 -> vertically maxed
 	#	- 2 -> vertically and horizontally maxed
 	#	- 3 -> fullscreen
+	# $4 indicates if multiple windows should be placed on successive desktops.
+
+	# Startup program and wait for launch.
 	$1 &
-	sleep 5s
-	WINID=$(wmctrl -l | grep -i $1 | cut -f1 -d " ")
-	echo "win id = " $WINID
-	echo "moving $1 to workspace $2"
-	wmctrl -i -r $WINID -t $2
-	sleep 1s
-	# change window size based on arguments
-	#	if 3, make full screen
-	if [[ $3 -eq 3 ]]
-       	then
-		echo "changing $1 to fullscreen"
-		wmctrl -i -r $WINID -b toggle,fullscreen
-	else
-		# if 2, make horizontally maxed
-		if [[ $3 -eq 2 ]]
+	sleep 9s
+	# Get process ids for running windows.
+	WINIDS=$(wmctrl -l | grep -i $1 | cut -f1 -d " ")
+	# For each process, move the program to the appropriate desktop.
+	INDEX=0
+	for WINID in $WINIDS
+	do
+		echo "win id = " $WINID
+		# get workspace number to move to.
+		WORKSPACE=$(($2+INDEX))
+		echo "moving $1 to workspace $WORKSPACE"
+		wmctrl -i -r $WINID -t $WORKSPACE
+		# increment index if applicable.
+		if [ "$4" = true ]
 		then
-			echo "changing $1 to maximized horz"
-			wmctrl -i -r $WINID -b toggle,maximized_horz
+			INDEX=$((INDEX+1))
 		fi
-		# if 1 or 2, make vertically maxed
-		echo "changing $1 to maximized vert"
-		wmctrl -i -r $WINID -b toggle,maximized_vert
-	fi
+		# Wait for successful move.
+		sleep 1s
+		# change window size based on arguments
+		#	if 3, make full screen
+		if [[ $3 -eq 3 ]]
+       		then
+			echo "changing $1 to fullscreen"
+			wmctrl -i -r $WINID -b toggle,fullscreen
+			sleep 1s
+		else
+			# if 2, make horizontally maxed
+			if [[ $3 -eq 2 ]]
+			then
+				echo "changing $1 to maximized horz"
+				wmctrl -i -r $WINID -b toggle,maximized_horz
+				sleep 1s
+			fi
+			# if 1 or 2, make vertically maxed
+			echo "changing $1 to maximized vert"
+			wmctrl -i -r $WINID -b toggle,maximized_vert
+		fi
+	done
 }
 
-launch firefox 8 2 &
-launch spotify 9 1 &
-launch slack 9 1 &
-launch code 1 3 &
+launch firefox 8 2 false &
+launch spotify 9 1 false &
+launch slack 9 1 false &
+launch code 1 3 true &
