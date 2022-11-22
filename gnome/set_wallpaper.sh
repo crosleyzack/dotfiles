8dddaff51b84a39397c7a13e22bbfd8be29cd148
zcrosley <zcrosley@censys.io>
2022-11-22 10:30:40 -0500

    Updated wallpaper options to zoom

--- gnome/set_wallpaper.sh
+++ gnome/set_wallpaper.sh
@@ -2,5 +2,5 @@
 
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
-gsettings set org.gnome.desktop.background picture-options "scaled"
+gsettings set org.gnome.desktop.background picture-options "zoom"
 gsettings set org.gnome.desktop.background show-desktop-icons "true"
