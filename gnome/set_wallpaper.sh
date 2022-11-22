56054b8f8ad75a6c10431361f8fccf313d0a5cbe
zcrosley <zcrosley@censys.io>
2022-11-22 09:31:10 -0500

    Updated wallpaper

---
+++ gnome/set_wallpaper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background picture-uri "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
+gsettings set org.gnome.desktop.background picture-options "scaled"
+gsettings set org.gnome.desktop.background show-desktop-icons "true"
