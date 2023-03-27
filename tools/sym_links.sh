3742f817ee151370d8f1ffe8caf28ed1247f00a6
crosleyzack <crosleyzack@gmail.com>
2023-03-27 16:53:34 -0400

    sym_links doesn't complain if files don't exist

--- tools/sym_links.sh
+++ tools/sym_links.sh
@@ -20,32 +20,32 @@ ln -s $1/alacritty/alacritty.yml $CONFIG/alacritty/alacritty.yml
 
 # VS Code
 mkdir -p $CONFIG/Code/User
-rm $CONFIG/Code/User/keybindings.json
+rm -f $CONFIG/Code/User/keybindings.json
 echo "Linking $CONFIG/Code/User/keybindings.json to $1/Code/User/keybindings.json"
 ln -s $1/Code/User/keybindings.json $CONFIG/Code/User/keybindings.json
-rm $CONFIG/Code/User/settings.json
+rm -f $CONFIG/Code/User/settings.json
 echo "Linking $CONFIG/Code/User/settings.json to $1/Code/User/settings.json"
 ln -s $1/Code/User/settings.json $CONFIG/Code/User/settings.json
 
 # Tmux
 mkdir -p $HOME/.tmux
-rm $HOME/.tmux/tmux.conf
+rm -f $HOME/.tmux/tmux.conf
 echo "Linking $HOME/.tmux/tmux.conf to $1/tmux/tmux.conf"
 ln -s $1/tmux/tmux.conf $HOME/.tmux/tmux.conf
 
 #Zsh
-rm $HOME/.zshrc
+rm -f $HOME/.zshrc
 echo "Linking $HOME/.zshrc to $1/zsh/zshrc"
 ln -s $1/zsh/zshrc $HOME/.zshrc
 
 # Docker
-sudo rm /etc/docker/daemon.json
+sudo rm -f /etc/docker/daemon.json
 echo "Linking /etc/docker/deamon.json to $1/docker/daemon.json"
 sudo ln -s $1/docker/daemon.json /etc/docker/daemon.json
 
 # GHC
 mkdir -p $HOME/.ghc
-rm $HOME/.ghc/ghci.conf
+rm -f $HOME/.ghc/ghci.conf
 echo "Linking $HOME/.ghc/ghci.conf to $1/haskell/ghci.conf"
 ln -s $1/haskell/ghci.conf $HOME/.ghc/ghci.conf
 chmod go-w $HOME/.ghc
@@ -53,23 +53,23 @@ chmod go-w $1/haskell/ghci.conf
 
 # Proxychains
 mkdir -p $HOME/.proxychains/
-rm $HOME/.proxychains/proxychains.conf
+rm -f $HOME/.proxychains/proxychains.conf
 echo "Linking $HOME/.proxychains/proxychains.conf to $1/proxychains/proxychains.conf"
 ln -s $1/proxychains/proxychains.conf $HOME/.proxychains/proxychains.conf
 
 # PulseAudio
 mkdir -p $HOME/.config/pulse/
-rm $HOME/.config/pulse/default.pa
+rm -f $HOME/.config/pulse/default.pa
 echo "Linking $HOME/.config/pulse/default.pa to $1/pulse/default.pa"
 ln -s $1/pulse/default.pa $HOME/.config/pulse/default.pa
 
 # Gitconfig - We actually copy this one so that we can change the user name!
-rm $HOME/.gitconfig
+rm -f $HOME/.gitconfig
 echo "copying $1/git/.gitconfig to $HOME/.gitconfig"
 cp $1/git/.gitconfig $HOME/.gitconfig
 
 # Vim
-rm $HOME/.vimrc
+rm -f $HOME/.vimrc
 echo "Linking $HOME/.vimrc to $1/vim/.vimrc"
 ln -s $1/vim/.vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim
