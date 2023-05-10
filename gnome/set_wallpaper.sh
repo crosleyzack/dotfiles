5bfacdaaca9b4b6b0a12c7691520472f43da35f8
crosleyzack <crosleyzack@gmail.com>
2023-05-10 11:35:40 -0400

    Removed wallpaper setup, moved to external repo

--- gnome/set_wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$HOME/dev/dotfiles/wallpaper/epictetus.jpg"
-gsettings set org.gnome.desktop.background picture-options "zoom"
-gsettings set org.gnome.desktop.background show-desktop-icons "true"
---
+++ wallpaper/README.md
@@ -0,0 +1,1 @@
+For current wallpaper setup, see https://github.com/CrosleyZack/random_desktop_quote
