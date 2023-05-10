f5539f4e4ad24742a51bfae8ea4467f2ebf03b95
crosleyzack <crosleyzack@gmail.com>
2023-05-10 16:00:22 -0400

    Updated Zsh setup to use spaceship vi

--- zsh/zsh_setup.sh
+++ zsh/zsh_setup.sh
@@ -1,9 +1,36 @@
+#!/bin/bash
+# Runs even if Zsh isn't already installed
+
 # Install zsh, if it isn't already.
 sudo apt install zsh
+
+# If ZSH isn't set, use default
+if [[ -z "${ZSH}" ]]; then
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    ZSH="$SCRIPT_DIR/oh-my-zsh"
+fi
+mkdir -p "$ZSH"
+if [[ -z "${ZSH_CUSTOM}" ]]; then
+    ZSH_CUSTOM="$ZSH/custom"
+fi
+mkdir -p "$ZSH_CUSTOM"
+
+# Setup oh my zsh
+rm -rf $ZSH
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$ZSH"
+
 # Make ZSH default shell.
 chsh -s $(which zsh)
-# Create zsh config directory, if it isn't already
-mkdir ~/.config/zsh
-cd ~/.config/zsh
-# Install zplugin
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/psprint/zplugin/master/doc/install.sh)"
+
+# Setup ZSH src - TODO, do not hardcode path
+rm -f $HOME/.zshrc
+echo "Linking $HOME/.zshrc to $HOME/dev/dotfiles/zsh/zshrc"
+ln -s $HOME/dev/dotfiles/zsh/zshrc $HOME/.zshrc
+# Install spaceship prompt
+rm -rf "$ZSH_CUSTOM/themes/spaceship-prompt"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Install spaceship vi
+rm -rf "$ZSH_CUSTOM/plugins/spaceship-vi-mode"
+git clone https://github.com/spaceship-prompt/spaceship-vi-mode.git "$ZSH_CUSTOM/plugins/spaceship-vi-mode"
