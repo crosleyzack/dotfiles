108bf9520c4a31c6a3404a32af582a242d15177e
crosleyzack <crosleyzack@gmail.com>
2023-09-27 11:04:02 -0400

    nix with home manager

---
+++ nix/install_nix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install nix package manager
+sh <(curl -L https://nixos.org/nix/install) --daemon
+# Add nixpkgs stable
+nix-channel --add https://channels.nixos.org/nixos-23.05 nixpkgs
+# Install home manager
+# sudo -i nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+# sudo -i nix-channel --update
