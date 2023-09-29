be3b2531871d27cd69bc74b6282416ecf0950b7d
crosleyzack <crosleyzack@gmail.com>
2023-09-29 17:07:24 -0400

    more configuration in home manager

--- nix/home.nix
+++ nix/home.nix
@@ -18,4 +18,83 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+      
+  home.packages = [
+    coreutils
+    pkgs.gcc
+    pkgs.cmake
+    pkgs.gnumake
+    pkgs.build-essential
+    pkgs.ca-certificates
+    pkgs.curl
+    pkgs.gnupg
+    pkgs.jq
+    pkgs.git 
+    pkgs.texlive-latex-extra
+    pkgs.texlive-fonts-extra
+    pkgs.vim
+    pkgs.fonts-powerline
+    pkgs.postgresql-client
+    pks.pipenv
+    pkgs.python3
+    pkgs.libssl-dev 
+    pkgs.libbz2-dev
+    pkgs.libsqlite3-dev
+    pkgs.wget
+    pkgs.llvm
+    pkgs.libncursesw5-dev
+    pkgs.tk-dev
+    pkgs.xsel
+    pkgs.net-tools
+    pkgs.ranger
+    pkgs.simple-scan
+    pkgs.whois
+    pkgs.rdesktop
+    pkgs.libtool
+    pkgs.libtool-bin
+    pkgs.gettext
+    pkgs.unzip
+    pkgs.vscode
+    zoom-us
+    pkgs.slack
+    pkgs.firefox
+    pkgs.gimp
+    pkgs.imagemagick
+    pkgs.gdb
+    pkgs.clips
+    pkgs.tor
+  ];
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "crosleyzack";
+    userEmail = "mail@crosleyzack.com";
+  };
+
+  programs.vim = {
+    enable = true;
+    extraConfig = builtins.readFile vim/vimrc;
+    settings = {
+       relativenumber = true;
+       number = true;
+    };
+    plugins = with pkgs.vimPlugins; [];
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      vscode-extension-github-copilot
+      vscode-extension-golang-Go
+      vscode-extension-ms-python-python
+      vscode-extension-ms-vscode-cmake-tools
+      vscode-extension-ms-vscode-makefile-tools
+      vscode-extension-GraphQL-vscode-graphql-syntax
+    ];
+    userSettings = {
+      "terminal.integrated.fontFamily" = "Hack";
+    };
+  };
 }
