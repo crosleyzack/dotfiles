11f7a9aca99474c40ed36f2b51a0b79e2b3d6f5c
crosleyzack <crosleyzack@gmail.com>
2023-10-04 14:21:21 -0400

    variable in tmux setup for abstraction

--- tmux/tmux_setup.sh
+++ tmux/tmux_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/.config/tmux
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tmux-resurrect ~/.config/tmux/plugins/resurrect
-git clone https://github.com/tmux-plugins/tmux-continuum ~/.config/tmux/plugins/continuum
-git clone https://github.com/erikw/tmux-powerline.git ~/.config/tmux/plugins/powerline
+PLUGINS_DIR="$HOME/.config/tmux/plugins"
+rm -rf $PLUGINS_DIR
+mkdir -p $PLUGINS_DIR
+git clone https://github.com/tmux-plugins/tpm $PLUGINS_DIR/tpm
+git clone https://github.com/tmux-plugins/tmux-resurrect $PLUGINS_DIR/resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum $PLUGINS_DIR/continuum
+git clone https://github.com/erikw/tmux-powerline.git $PLUGINS_DIR/powerline
