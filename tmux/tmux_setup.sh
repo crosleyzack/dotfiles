6fbdddb78908123c1fe1977b9c7eb8603b376f92
crosleyzack <crosleyzack@gmail.com>
2023-10-04 09:35:38 -0400

    fixed tmux setup

--- tmux/tmux_setup.sh
+++ tmux/tmux_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir ~/.config/tmux
+mkdir -p ~/.config/tmux
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/.config/tmux/plugins/resurrect
 git clone https://github.com/tmux-plugins/tmux-continuum ~/.config/tmux/plugins/continuum
 git clone https://github.com/erikw/tmux-powerline.git ~/.config/tmux/plugins/powerline
