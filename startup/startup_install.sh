fc55cd92883b3abe227e79280e22bdb20e211f09
crosleyzack <crosleyzack@gmail.com>
2023-10-04 12:33:24 -0400

    installer generates startup.desktop for startup script

--- startup/startup_install.sh
+++ startup/startup_install.sh
@@ -5,9 +5,19 @@ FILE_PATH=$(realpath $BASH_SOURCE)
 DIR_PATH=$(dirname $FILE_PATH)
 # echo "DIR_PATH = $DIR_PATH"
 
+# Create startup.desktop
+DESKTOP_FILE="$DIR_PATH/startup.desktop"
+rm -f $DESKTOP_FILE
+echo "[Desktop Entry]
+Type=Application
+Name=StartupScript
+Exec=$DIR_PATH/startup_program.sh
+OnlyShowIn=GNOME;" >> $DESKTOP_FILE
+
+# link to generated startup script.
 mkdir -p $HOME/.config/autostart
 rm -f $HOME/.config/autostart/startup.desktop
-ln -s $DIR_PATH/startup.desktop $HOME/.config/autostart/startup.desktop
+ln -s $DESKTOP_FILE $HOME/.config/autostart/startup.desktop
 # cp "$DIR_PATH/startup.desktop" $HOME/.config/autostart
 
 ### OLD attempt crontab
