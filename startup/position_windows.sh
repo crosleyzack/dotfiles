012fdd195fcf21cf32c9f0864995a901f5918222
crosleyzack <crosleyzack@gmail.com>
2023-10-06 13:09:51 -0400

    Separated startup launching of programs and repositioning

---
+++ startup/position_windows.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# To use, set as a program to run on OS start. This can be done on Ubuntu
+#	in Applications > Statup Applications
+# Note: This script should work on any X Window Manager system.
+
+# Get desktop size. See https://askubuntu.com/questions/584688/how-can-i-get-the-monitor-resolution-using-the-command-line
+X=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+Y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+
+function position {
+	# $1 indicates program to position - must be name of executable in bash path
+	# $2 indicates workspace to put on (0 indexed)
+	# $3 indicates desired window state:
+	#	- 1 -> vertically maxed
+	#	- 2 -> vertically and horizontally maxed
+	#	- 3 -> fullscreen
+	# $4 indicates if multiple windows should be placed on successive desktops.
+	# $5 if $3==1, horizontal space for window to use
+	# $6 if $3==1, X placement of window
+
+	# Get process ids for running windows.
+	WINIDS=$(wmctrl -l | grep -i $1 | cut -f1 -d " ")
+	# For each process, move the program to the appropriate desktop.
+	INDEX=0
+	for WINID in $WINIDS
+	do
+		echo "win id = " $WINID
+		# get workspace number to move to.
+		WORKSPACE=$(($2+INDEX))
+		echo "moving $1 to workspace $WORKSPACE"
+		wmctrl -i -r $WINID -t $WORKSPACE
+		# increment index if applicable.
+		if [ "$4" = true ]
+		then
+			INDEX=$((INDEX+1))
+		fi
+		# Wait for successful move.
+		sleep 1s
+		# change window size based on arguments
+		#	if 3, make full screen
+		if [[ $3 -eq 3 ]]; then
+			echo "changing $1 to fullscreen"
+			wmctrl -i -r $WINID -b toggle,fullscreen
+		elif [[ $3 -eq 2 ]]; then
+			echo "changing $1 to maximized horz"
+			wmctrl -i -r $WINID -e 0,0,0,$X,$Y
+		elif [[ $3 -eq 1 ]]; then
+			# if 1 make vertically maxed
+			echo "changing $1 to maximized vert"
+			wmctrl -i -r $WINID -e 0,$6,0,$5,$Y
+		fi
+		sleep 0.33
+	done
+}
+
+position firefox 8 2 false &
+THIRD=$(( $X / 3 ))
+HALF=$(( $X / 2 ))
+position spotify 9 1 false $HALF 0 &
+position slack 9 1 false $HALF $HALF &
+position code 1 3 true &
--- startup/startup_program.sh
+++ startup/startup_program.sh
@@ -1,67 +1,18 @@
 #!/bin/bash
 
-# To use, set as a program to run on OS start. This can be done on Ubuntu
-#	in Applications > Statup Applications
-# Note: This script should work on any X Window Manager system.
+FILE_PATH=$(realpath $BASH_SOURCE)
+DIR=$(dirname $FILE_PATH)
 
-# Get desktop size. See https://askubuntu.com/questions/584688/how-can-i-get-the-monitor-resolution-using-the-command-line
-X=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-Y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+PATH="$PATH:/usr/local/bin:/bin:/usr/bin:$HOME/.nix-profile/bin/"
 
-function launch {
-	# $1 indicates program to launch - must be name of executable in bash path
-	# $2 indicates workspace to put on (0 indexed)
-	# $3 indicates desired window state:
-	#	- 1 -> vertically maxed
-	#	- 2 -> vertically and horizontally maxed
-	#	- 3 -> fullscreen
-	# $4 indicates if multiple windows should be placed on successive desktops.
-	# $5 if $3==1, horizontal space for window to use
-	# $6 if $3==1, X placement of window 
+firefox > /dev/null &
+spotify > /dev/null &
+slack > /dev/null &
+code > /dev/null &
+alacritty -e tmux > /dev/null &
 
-	# Startup program and wait for launch.
-	$1 &
-	sleep 9s
-	# Get process ids for running windows.
-	WINIDS=$(wmctrl -l | grep -i $1 | cut -f1 -d " ")
-	# For each process, move the program to the appropriate desktop.
-	INDEX=0
-	for WINID in $WINIDS
-	do
-		echo "win id = " $WINID
-		# get workspace number to move to.
-		WORKSPACE=$(($2+INDEX))
-		echo "moving $1 to workspace $WORKSPACE"
-		wmctrl -i -r $WINID -t $WORKSPACE
-		# increment index if applicable.
-		if [ "$4" = true ]
-		then
-			INDEX=$((INDEX+1))
-		fi
-		# Wait for successful move.
-		sleep 1s
-		# change window size based on arguments
-		#	if 3, make full screen
-		if [[ $3 -eq 3 ]]; then
-			echo "changing $1 to fullscreen"
-			wmctrl -i -r $WINID -b toggle,fullscreen
-		elif [[ $3 -eq 2 ]]; then
-			echo "changing $1 to maximized horz"
-			wmctrl -i -r $WINID -e 0,0,0,$X,$Y
-		elif [[ $3 -eq 1 ]]; then
-			# if 1 make vertically maxed
-			echo "changing $1 to maximized vert"
-			wmctrl -i -r $WINID -e 0,$6,0,$5,$Y
-		fi
-		sleep 1s
-	done
-}
+echo "Programs launched, sleeping"
+sleep 7
+echo "Sleep done, repositioning windows via $DIR/position_windows.sh"
 
-launch firefox 8 2 false &
-#THIRD=$(( $X / 3 ))
-HALF=$(( $X / 2 ))
-launch spotify 9 1 false $HALF 0 &
-# launch slack 9 1 false $(( $X - $THIRD )) $THIRD &
-launch code 1 3 true &
-# Launch alacritty - cannot move via code above, as it doesn't show up in wmcrtl
-alacritty -e tmux
+exec $DIR/position_windows.sh
