76080fbb9686f2a336500cc3104d47769a74881b
crosleyzack <crosleyzack@gmail.com>
2023-10-09 17:47:31 -0400

    Added separate firefox installer

---
+++ debian/install_firefox.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source "${BASH_SOURCE%/*}/../tools/install_tools.sh"
+
+# Setup firefox. See https://support.mozilla.org/en-US/kb/install-firefox-linux
+is_installed firefox
+if [ "true" = "$INSTALLED" ]
+then
+    sudo rm -rf /opt/firefox
+    sudo mkdir -p /opt
+    mkdir -p $HOME/temp
+    rm -f $HOME/temp/firefox.bz2
+    curl -L -o $HOME/temp/firefox.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+    cd $HOME/temp && tar xjf firefox.bz2
+    sudo cp -r $HOME/temp/firefox /opt
+    rm $HOME/temp/firefox.bz2
+    sudo rm /usr/local/bin/firefox
+    sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+else
+    echo "firefox already installed! Skipping"
+fi
+
+
