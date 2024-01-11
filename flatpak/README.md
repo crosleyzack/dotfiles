37ce4bdb657633e3c949fb69df08ba52c3f486d8
crosleyzack <crosleyzack@gmail.com>
2024-01-11 16:13:18 -0500

    Added flatpaks for future use

---
+++ flatpak/README.md
@@ -0,0 +1,1 @@
+Currently unused, possible future use in place of nix
---
+++ flatpak/installs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub io.podman_desktop.PodmanDesktop
+flatpak install flathub org.gimp.GIMP
+flatpak install flathub com.spotify.Client
+flatpak install flathub org.signal.Signal
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.bitwarden.desktop
