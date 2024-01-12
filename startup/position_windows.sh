e936a3efdb9b9a7444451e80b5ed2c4dd81eb1f4
crosleyzack <crosleyzack@gmail.com>
2024-01-12 09:49:47 -0500

    clearer output for position windows

--- startup/position_windows.sh
+++ startup/position_windows.sh
@@ -30,6 +30,9 @@ function position {
 
 	# Get process ids for running windows.
 	WINIDS=$(wmctrl -l | grep -i $1 | cut -f1 -d " ")
+    if [ -z "$WINIDS" ]; then
+        echo "WARN: no Windows found for process $1"
+    fi
 	# For each process, move the program to the appropriate desktop.
 	INDEX=0
 	for WINID in $WINIDS
@@ -48,14 +51,14 @@ function position {
 		sleep 1s
 		# change window size based on arguments
 		#	if 3, make full screen
-		if [[ $3 -eq 3 ]]; then
+		if [[ $3 -eq $FULLSCREEN ]]; then
 			echo "changing $1 to fullscreen"
 			wmctrl -i -r $WINID -b toggle,fullscreen
-		elif [[ $3 -eq 2 ]]; then
+		elif [[ $3 -eq $VERT_HORZ_MAXED ]]; then
 			echo "changing $1 to maximized horz"
             # The -e option expects a list of comma separated integers: "gravity,X,Y,width,height"
 			wmctrl -i -r $WINID -e 0,0,0,$X,$Y
-		elif [[ $3 -eq 1 ]]; then
+		elif [[ $3 -eq $VERT_MAXED ]]; then
 			# if 1 make vertically maxed
 			echo "changing $1 to maximized vert"
 			wmctrl -i -r $WINID -e 0,$6,0,$5,$Y
@@ -75,11 +78,15 @@ FULLSCREEN=3
 # NOTE: looks like location might require calculation
 #  see https://github.com/jc00ke/move-to-next-monitor/blob/master/move-to-next-monitor
 
-position zoom 7 $VERT_MAXED false $HALF 0 &
-position docker 7 $VERT_MAXED false $HALF $HALF &
-position firefox 8 $VERT_HORZ_MAXED false $X $Y &
 THIRD=$(( $X / 3 ))
 HALF=$(( $X / 2 ))
-position spotify 9 $VERT_MAXED false $HALF 0 &
-position slack 9 $VERT_MAXED false $HALF $HALF &
-position code 1 $FULLSCREEN true $X $Y &
+#        program desktop_id
+#                  window_size      array_windows
+#                                         xSize ySize
+position zoom    7 $VERT_MAXED      false $HALF 0 &
+position docker  7 $VERT_MAXED      false $HALF $HALF &
+position podman  7 $VERT_MAXED      false $HALF 0 &
+position firefox 8 $VERT_HORZ_MAXED false $X    $Y &
+position spotify 9 $VERT_MAXED      false $HALF 0 &
+position slack   9 $VERT_MAXED      false $HALF $HALF &
+position code    1 $FULLSCREEN      true  $X    $Y &
