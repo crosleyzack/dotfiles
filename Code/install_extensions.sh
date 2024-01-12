a97288ac9403fa2c2c141c199d8d19d004e7c424
crosleyzack <crosleyzack@gmail.com>
2024-01-12 16:17:32 -0500

    Code fixesg

--- Code/install_extensions.sh
+++ Code/install_extensions.sh
@@ -2,4 +2,8 @@
 
 code --install-extension golang.go
 code --install-extension asvetliakov.vscode-neovim
+# Required link for nvim to work
+sudo ln -s $(which nvim) /usr/local/bin/nvim
 code --install-extension GitHub.copilot
+code --install-extension ms-python.python
+code --install-extension zxh404.vscode-proto3
