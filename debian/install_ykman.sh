84f0f4e920467e638f9ba2f846b3e4b7e890bdf6
crosleyzack <crosleyzack@gmail.com>
2024-01-16 15:02:51 -0500

    remove ykman before install

--- debian/install_ykman.sh
+++ debian/install_ykman.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "WARN this lacks the ability to update because debian"
+
+# Remove old
+sudo apt-add-repository --remove ppa:yubico/stable
+sudo apt remove yubikey-manager
+# install new
 sudo apt install python3-launchpadlib
 sudo apt-add-repository ppa:yubico/stable
 sudo apt update
