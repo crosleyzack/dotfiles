99cea2f497ace7fd3528ad4b99e797dff605cb9f
crosleyzack <crosleyzack@gmail.com>
2024-01-17 09:53:31 -0500

    fedora installer for docker / kubs

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -3,30 +3,31 @@
 source "${BASH_SOURCE%/*}/../tools/install_tools.sh"
 
 # Setup docker. See https://docs.docker.com/engine/install/debian
-is_installed docker-ce
+is_installed docker
 if [ "false" = "$INSTALLED" ]
 then
-    dpkg -l | grep -i docker
-    sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
-    sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
+    # dpkg -l | grep -i docker
+    # sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
+    # sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
+    sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
     sudo rm -rf /var/lib/docker /etc/docker
     sudo rm -f /etc/apparmor.d/docker
     sudo groupdel docker
     sudo rm -rf /var/run/docker.sock
     # Add Docker's official GPG key:
-    # ansible localhost -m ansible.builtin.package -a "name=curl state=latest" -a "name=gnupg state=latest" -a "name=ca-certificates state=latest"
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    ansible localhost -m ansible.builtin.package -a "name=curl state=latest" -a "name=gnupg state=latest" -a "name=ca-certificates state=latest"
+    # sudo apt-get update
+    # sudo apt-get install ca-certificates curl gnupg
+    # sudo install -m 0755 -d /etc/apt/keyrings
+    # curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    # sudo chmod a+r /etc/apt/keyrings/docker.gpg
     # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # sudo apt-get update
+    # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     echo "docker installed"
     # add user to docker group
     sudo groupadd -f docker
@@ -42,7 +43,9 @@ then
     # sudo ln -s $BASE_DIR/docker/daemon.json /etc/docker/daemon.json
     # launch with system: https://docs.docker.com/engine/install/linux-postinstall/#configure-docker-to-start-on-boot-with-systemd
     sudo systemctl enable docker.service
+    sudo systemctl start docker
     sudo systemctl enable containerd.service
+    sudo systemctl start containerd
     echo "docker-ce service enabled!"
 else
     echo "docker-ce already installed! Skipping"
@@ -58,28 +61,28 @@ then
     # echo "docker compose \"$@\"" > /usr/bin/docker-compose
     echo "docker-compose installed!"
 else
-     echo "docker-compose already installed! Skipping"
+    echo "docker-compose already installed! Skipping"
 fi
 
 # Setup docker desktop. https://docs.docker.com/desktop/install/debian/
-is_installed docker-desktop
-if [ "false" = "$INSTALLED" ]
-then
-    # remove old
-    rm -rf $HOME/.docker/desktop
-    sudo rm -f /usr/local/bin/com.docker.cli
-    sudo apt purge docker-desktop
-    # # install curl if not already
-    sudo apt-get update
-    sudo apt-get install curl --yes
-    # # get new copy of deb file
-    mkdir -p $HOME/temp
-    rm -f $HOME/temp/docker-desktop.deb
-    curl -SL -o $HOME/temp/docker-desktop.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.1-amd64.deb
-    # install deb file
-    sudo dpkg -i $HOME/temp/docker-desktop.deb
-    # # set to run at startup
-    systemctl --user enable docker-desktop
-else
-    echo "docker-desktop already installed! Skipping"
-fi
+# is_installed docker-desktop
+# if [ "false" = "$INSTALLED" ]
+# then
+#     # remove old
+#     rm -rf $HOME/.docker/desktop
+#     sudo rm -f /usr/local/bin/com.docker.cli
+#     sudo apt purge docker-desktop
+#     # # install curl if not already
+#     sudo apt-get update
+#     sudo apt-get install curl --yes
+#     # # get new copy of deb file
+#     mkdir -p $HOME/temp
+#     rm -f $HOME/temp/docker-desktop.deb
+#     curl -SL -o $HOME/temp/docker-desktop.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.1-amd64.deb
+#     # install deb file
+#     sudo dpkg -i $HOME/temp/docker-desktop.deb
+#     # # set to run at startup
+#     systemctl --user enable docker-desktop
+# else
+#     echo "docker-desktop already installed! Skipping"
+# fi
--- docker/install_kubernetes.sh
+++ docker/install_kubernetes.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "WARNING! If you are using nix, you don't want to install this! Backout now!"
-sleep 10
-echo "WARNING! Was not cancelled, continuing!"
-
+# echo "WARNING! If you are using nix, you don't want to install this! Backout now!"
+# sleep 10
+# echo "WARNING! Was not cancelled, continuing!"
 
 source "${BASH_SOURCE%/*}/../tools/install_tools.sh"
 
@@ -11,19 +10,32 @@ source "${BASH_SOURCE%/*}/../tools/install_tools.sh"
 is_installed kubectl
 if [ "false" = "$INSTALLED" ]
 then
-    sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/   kubernetes.list
-    sudo apt-get update
-    sudo apt-get install -y kubectl
+    # This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
+    # TODO fix this
+    cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+EOF
+    sudo yum install -y  kubectl
+    # sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    # echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    # sudo apt-get update
+    # sudo apt-get install -y kubectl
+    echo "kubectl installed"
 fi
 
 # Install Kubernetes kind. See https://kind.sigs.k8s.io/
 is_installed kind
 if [ "false" = "$INSTALLED" ]
 then
-    go install sigs.k8s.io/kind@v0.17.0
+    go install sigs.k8s.io/kind@latest
     # Make sure go in path. Should already be true
     # export PATH=$PATH:$($(go env GOPATH)/bin)
+    echo "kind installed"
 fi
 
 # Install Kubernetes ctlptl. See https://github.com/tilt-dev/ctlptl/blob/main/INSTALL.md
@@ -33,27 +45,31 @@ then
     go install github.com/tilt-dev/ctlptl/cmd/ctlptl@latest
     # Make sure go in path. Should already be true
     # export PATH=$PATH:$($(go env GOPATH)/bin)
+    echo "ctlptl installed"
 fi
 
 # Install Kubernetes helm. See https://helm.sh/docs/intro/install/#from-apt-debianubuntu
 is_installed helm
 if [ "false" = "$INSTALLED" ]
 then
-    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-    sudo apt-get install apt-transport-https --yes
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-    sudo apt-get update
-    sudo apt-get install helm
+    sudo dnf install helm
+    # curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    # sudo apt-get install apt-transport-https --yes
+    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    # sudo apt-get update
+    # sudo apt-get install helm
+    echo "helm installed"
 fi
 
 # Install Kubernetes kubectx. https://github.com/ahmetb/kubectx
 is_installed kubectx
 if [ "false" = "$INSTALLED" ]
 then
-    sudo apt install kubectx
-    # sudo git clone https://github.com/ahmetb/kubectx /usr/local/kubectx
-    # sudo ln -s /usr/local/kubectx/kubectx /usr/local/bin/kubectx
-    # sudo ln -s /usr/local/kubectx/kubens /usr/local/bin/kubens
+    # sudo apt install kubectx
+    sudo git clone https://github.com/ahmetb/kubectx /usr/local/kubectx
+    sudo ln -s /usr/local/kubectx/kubectx /usr/local/bin/kubectx
+    sudo ln -s /usr/local/kubectx/kubens /usr/local/bin/kubens
+    echo "kubectx installed"
 fi
 
 # Install tilt: https://docs.tilt.dev/install.html#linux
@@ -61,32 +77,48 @@ is_installed tilt
 if [ "false" = "$INSTALLED" ]
 then
     curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+    echo "tilt installed"
 fi
 
 # Install task: https://taskfile.dev/installation/#go-modules
-is_installed tilt
+is_installed task
 if [ "false" = "$INSTALLED" ]
 then
     go install github.com/go-task/task/v3/cmd/task@latest
+    echo "task installed"
 fi
 
 # https://cloud.google.com/sdk/docs/install#deb
 is_installed google-cloud-cli
 if [ "false" = "$INSTALLED" ]
 then
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get update && sudo apt-get install google-cloud-cli
+    # curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    # sudo apt-get update && sudo apt-get install google-cloud-cli
+    sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+    sudo dnf install libxcrypt-compat.x86_64
+    sudo dnf install google-cloud-cli
+    echo "gcloud cli installed"
 fi
 
 # https://github.com/wagoodman/dive#installation
 is_installed dive
 if [ "false" = "$INSTALLED" ]
 then
-    export DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-    mkdir -p $HOME/temp
-    curl -L -o $HOME/temp/dive.deb https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
-    sudo apt install $HOME/temp/dive.deb
+    # export DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+    # mkdir -p $HOME/temp
+    # curl -L -o $HOME/temp/dive.deb https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
+    # sudo apt install $HOME/temp/dive.deb
+    go install github.com/wagoodman/dive@latest
+    echo "dive installed"
 fi
 
 is_installed cloud-sql-proxy
@@ -98,4 +130,5 @@ then
     chmod +x $HOME/bin/cloud-sql-proxy
     sudo rm -f /usr/local/bin/cloud-sql-proxy
     sudo ln -s $HOME/bin/cloud-sql-proxy /usr/local/bin/cloud-sql-proxy
+    echo "cloud sql proxy installed"
 fi
