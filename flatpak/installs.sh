e0137f1dd4e28d9569dab43c53f8ebb14cb76328
crosleyzack <crosleyzack@gmail.com>
2024-01-17 09:13:34 -0500

    removed flatpaks that are better installed otherwise

--- flatpak/installs.sh
+++ flatpak/installs.sh
@@ -4,8 +4,9 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 flatpak install flathub io.podman_desktop.PodmanDesktop
 flatpak install flathub org.gimp.GIMP
 flatpak install flathub com.spotify.Client
+flatpak install flathub com.slack.Slack
 flatpak install flathub org.signal.Signal
-flatpak install flathub com.visualstudio.code
+# flatpak install flathub com.visualstudio.code
 flatpak install flathub com.bitwarden.desktop
-flatpak install flathub io.neovim.nvim
-flatpak install flathub org.mozilla.firefox
+# flatpak install flathub io.neovim.nvim
+# flatpak install flathub org.mozilla.firefox
