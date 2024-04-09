84ed559fdc5624c9a80c9c7639c428a4b8f78e5b
crosleyzack <zcrosley@censys.com>
2024-04-09 11:14:50 -0400

    limit extensions

--- Code/install_extensions.sh
+++ Code/install_extensions.sh
@@ -7,4 +7,4 @@ sudo ln -s $(which nvim) /usr/local/bin/nvim
 code --install-extension GitHub.copilot
 code --install-extension ms-python.python
 code --install-extension zxh404.vscode-proto3
-code --install-extension ms-azuretools.vscode-docker
+# code --install-extension ms-azuretools.vscode-docker
