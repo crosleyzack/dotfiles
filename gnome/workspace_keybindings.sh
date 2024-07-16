280e893efddd45a93fe2b73f842f6d3eb060fb1e
crosleyzack <zcrosley@censys.com>
2024-07-16 12:20:28 -0400

    Better i3 type keybindings for gnome

--- gnome/workspace_keybindings.sh
+++ gnome/workspace_keybindings.sh
@@ -14,6 +14,7 @@ dconf reset /org/gnome/shell/extensions/dash-to-dock/hot-keys
 #    dconf dump /org/gnome/desktop/wm/keybindings/
 #	remove mapping
 #	org.gnome.shell.keybindings switch-to-application-1 ['<Super>1']
+echo "Removing switch to application # keybindings"
 dconf reset /org/gnome/desktop/wm/keybindings/switch-to-application-1
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Super>Grave']"
 dconf reset /org/gnome/desktop/wm/keybindings/switch-to-application-2
@@ -36,6 +37,7 @@ dconf reset /org/gnome/desktop/wm/keybindings/switch-to-application-10
 # gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super>Grave']"
 
 # Set super + num goes to workspace num.
+echo "Set change workspace keybindings"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
@@ -48,6 +50,7 @@ dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>9'
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Super>0']"
 
 # Set super + shift + num moves a window to workspace num.
+echo "Set move to workspace keybindings"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-1  "['<Super><Shift>1']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-2  "['<Super><Shift>2']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-3  "['<Super><Shift>3']"
@@ -59,26 +62,79 @@ dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8  "['<Super><Sh
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-9  "['<Super><Shift>9']"
 dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-10 "['<Super><Shift>0']"
 
-echo "Set workspace keyboard shortcuts"
+# Unbind applicaiton switch, set to cycle in workspace
+echo "Set cycle within workspace binding"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-applications
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-applications-backward
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Super>Grave']"
+dconf write /org/gnome/desktop/wm/keybindings/cycle-windows "['<Super>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/cycle-windows-backward "['<Super><Shift>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/cycle-group "['<Super>a']"
+dconf write /org/gnome/desktop/wm/keybindings/cycle-group-backward "['<Super><Shift>a']"
+
+# Move to side
+echo "Set move application to side keybindings"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-side-e "['<Super>l']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-side-n "['<Super>k']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-side-s "['<Super>j']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-side-w "['<Super>h']"
+dconf write /org/gnome/desktop/wm/keybindings/maximize-horizontally "['<Super>s']"
+dconf write /org/gnome/desktop/wm/keybindings/maximize-vertically "['<Super>v']"
 
 # Use vim bindings for maximize too
-dconf write /org/gnome/desktop/wm/keybindings/maximize "['<Super>Up','<Super>k']"
-dconf write /org/gnome/desktop/wm/keybindings/minimize "['<Super><Shift>Down','<Super><Shift>j']"
-dconf write /org/gnome/desktop/wm/keybindings/unmaximize "['<Super>Down', '<Super>j']"
-dconf write /org/gnome/desktop/wm/keybindings/toggle-fullscreen "['<Super>z']"
+echo "Set maximize keybindings"
+dconf reset /org/gnome/desktop/wm/keybindings/minimize
+gsettings set org.gnome.desktop.wm.keybindings minimize "['<Super>Grave']"
+dconf write /org/gnome/desktop/wm/keybindings/maximize "['<Super>Up']"
+dconf write /org/gnome/desktop/wm/keybindings/unmaximize "['<Super>Down']"
+dconf write /org/gnome/desktop/wm/keybindings/toggle-fullscreen "['<Super>f']"
 
 # Unbind move monitor
+echo "Removing unnecessary bindings"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-to-workspace-left
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-to-workspace-right
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-to-workspace-last
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-last "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-to-workspace-up
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-to-workspace-down
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-workspace-left
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-workspace-right
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-workspace-last
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-last "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-workspace-up
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-workspace-down
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super>Grave']"
 dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-left
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "['<Super>Grave']"
 dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-right
-# Move to adjacent workspaces
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-left "['<Super><Shift>h','<Super><Shift>Left']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-right "['<Super><Shift>l','<Super><Shift>Right']"
-dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-left "['<Super><Shift><Alt>h','<Super><Shift><Alt>Left']"
-dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-right "['<Super><Shift><Alt>l','<Super><Shift><Alt>Right']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-up
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-up "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/move-to-monitor-down
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-down "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-group
+gsettings set org.gnome.desktop.wm.keybindings switch-group "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-group-backward
+gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-group-panels
+gsettings set org.gnome.desktop.wm.keybindings switch-group-panels "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-group-panels-backward
+gsettings set org.gnome.desktop.wm.keybindings switch-group-panels-backward "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-panels
+gsettings set org.gnome.desktop.wm.keybindings switch-panels "['<Super>Grave']"
+dconf reset /org/gnome/desktop/wm/keybindings/switch-panels-backward
+gsettings set org.gnome.desktop.wm.keybindings switch-panels-backward "['<Super>Grave']"
 
 echo "Set other keybindings"
 
-#
 # org.gnome.desktop.default-applications.terminal exec 'x-terminal-emulator'
 # org.gnome.desktop.default-applications.terminal exec-arg '-e'
 # TODO Set super + enter to open terminal.
