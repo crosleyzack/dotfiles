becf204773134f67a01df19a00e299e607570ae0
crosleyzack <crosleyzack@gmail.com>
2024-12-27 09:10:54 -0500

    Vscode remove leader and use high contrast theme

--- Code/install_extensions.sh
+++ Code/install_extensions.sh
@@ -11,6 +11,7 @@ code --install-extension ms-python.python
 # code --install-extension ms-python.black-formatter
 code --install-extension zxh404.vscode-proto3
 code --install-extension ms-vscode-remote.remote-containers
+code --install-extension GitHub.github-vscode-theme
 # code --install-extension ms-azuretools.vscode-docker
 # Scala - disable by default
 # code --install-extension scalameta.metals
