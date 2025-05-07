9f23a4883dc7d75b8c98bbffbf1599e9ef26675e
crosleyzack <crosleyzack@gmail.com>
2025-05-07 10:58:15 -0400

    update golang version

--- golang/install.sh
+++ golang/install.sh
@@ -2,9 +2,10 @@
 
 # https://go.dev/doc/install
 mkdir -p $HOME/temp && rm -f $HOME/temp/go.tar.gz
-curl -L -o $HOME/temp/go.tar.gz https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+curl -L -o $HOME/temp/go.tar.gz https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf $HOME/temp/go.tar.gz
+sudo chown -R $USER /usr/local/go
 export PATH=$PATH:/usr/local/go/bin
 
 # golang tools
