d997a62b7213a534a70b54750857519984237382
crosleyzack <crosleyzack@gmail.com>
2025-05-23 09:34:02 -0400

    feat(toolbx): fix host spawn

--- toolbox/rebuild.sh
+++ toolbox/rebuild.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+FILE_PATH=$(realpath $BASH_SOURCE)
+DIR=$(dirname $FILE_PATH)
+
 podman stop devs
 toolbox rm devs
 toolbox rmi devs-image
-podman build -t devs-image --label=devbox -f Dockerfile .
+podman build -t devs-image --label=devbox -f "$DIR/Dockerfile" .
 toolbox create --image devs-image devs
