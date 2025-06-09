03801cec14828a826813bc428e8284d2f20aad53
crosleyzack <crosleyzack@gmail.com>
2025-06-08 22:00:34 -0400

    specify go path for toolbox

--- toolbox/rebuild.sh
+++ toolbox/rebuild.sh
@@ -3,8 +3,9 @@
 FILE_PATH=$(realpath $BASH_SOURCE)
 DIR=$(dirname $FILE_PATH)
 
-podman stop devs
-toolbox rm devs
-toolbox rmi devs-image
+podman stop -i devs
+toolbox rm -f devs
+toolbox rmi -f devs-image
+podman rmi -f devs-image
 podman build -t devs-image --label=devbox -f "$DIR/Dockerfile" .
 toolbox create --image devs-image devs
