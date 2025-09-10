fb02810bac68a8d0a91a86072354ad895a3246ae
crosleyzack <crosleyzack@gmail.com>
2025-09-10 09:19:49 -0400

    nix installer updates

--- nix/install_nix.sh
+++ nix/install_nix.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # NOTE: this can also be run to update nix version. Simply change `23.11` below to the appropriate value
+VERSION="23.11"
 
 # Install nix package manager
 NIX_PATH=$(which nix)
 if [[ -z "$NIX_PATH" ]]; then
     echo "NIX not installed, installing"
-    sh <(curl -L https://nixos.org/nix/install) --daemon
+    sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 fi
 # Add nixpkgs stable and unstable
 sudo -i nix-channel --remove nixpkgs
-sudo -i nix-channel --add https://channels.nixos.org/nixos-23.11 nixpkgs
+sudo -i nix-channel --add "https://channels.nixos.org/nixos-$VERSION" nixpkgs
 sudo -i nix-channel --remove unstable
 sudo -i nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
 # Install home manager
 sudo -i nix-channel --remove home-manager
-sudo -i nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
+sudo -i nix-channel --add "https://github.com/nix-community/home-manager/archive/release-$VERSION.tar.gz" home-manager
 sudo -i nix-channel --update
 # setup home manager
 echo "Home manager installed, the command to setup may not work until after a restart"
