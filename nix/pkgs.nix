7cb382d797b30d0d816effe1e98e16a5027ce421
crosleyzack <crosleyzack@gmail.com>
2025-10-29 19:54:38 -0400

    add crane to nix

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -2,5 +2,5 @@
 # NOTE: install zsh via your system package manager currently
 # TODO use zsh plugins zsh-autosuggestions spaceship-prompt zsh-vi-mode zsh-syntax-highlighting
 with import <nixpkgs>{}; [
-	vscode tmux jq yq libgcc direnv cmake git git-lfs tor proxychains nmap helm kubectl go golangci-lint gopls delve go-task dive go protobuf grpcurl protoc-gen-go protoc-gen-go-grpc ctlptl buf man-db kind htop atuin tldr nix-info wmctrl xorg.xrandr yubikey-manager
+	vscode tmux jq yq libgcc direnv cmake git git-lfs tor proxychains nmap helm kubectl go golangci-lint gopls delve go-task dive go protobuf grpcurl protoc-gen-go protoc-gen-go-grpc ctlptl buf man-db kind htop atuin tldr nix-info wmctrl xorg.xrandr yubikey-manager crane
 ]
