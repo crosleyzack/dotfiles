e7ebb4ae742979f11f3b72f8a5dba73482a74cbf
crosleyzack <crosleyzack@gmail.com>
2025-12-05 10:53:53 -0500

    simplify update script

--- utils/update.sh
+++ utils/update.sh
@@ -2,13 +2,8 @@
 
 # Script to update machine fully
 
-FILE="${BASH_SOURCE%/*}"
-source "$FILE/../utils/installed.sh"
-
 # Apt packages
-is_installed apt
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v apt)" ]; then
     sudo apt update
     sudo apt dist-upgrade
     sudo apt autoremove
@@ -17,81 +12,65 @@ then
 fi
 
 # dnf packages
-is_installed dnf
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v dnf)" ]; then
     sudo dnf upgrade
     sudo dnf distro-sync
 fi
 
 # ansible
-is_installed ansible
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v ansible)" ]; then
     ansible-playbook -b "$FILE/../ansible/packages.yaml" --ask-become-pass
 fi
 
 # Update rust
-is_installed rustup
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v rustup)" ]; then
     rustup update
 fi
 
 # Update brew
-is_installed brew
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v brew)" ]; then
     brew update
     brew upgrade
 fi
 
 # Update important python libs
-is_installed python
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v python)" ]; then
     python -m pip install --upgrade wheel pip setuptools virtualenv
 fi
 
 # update snap
-is_installed snap
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v snap)" ]; then
     sudo snap refresh
 fi
 
 # update nix
-is_installed nix-channel
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v nix-channel)" ]; then
     nix-channel --update
+    # run periodically
     # nix-collect-garbage -d
 fi
 
+# home manager update
+if [ -x "$(command -v home-manager)" ]; then
+    home-manager switch
+fi
+
 # update flatpak
-is_installed flatpak
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v flatpak)" ]; then
     flatpak update
 fi
 
 # update vscode extensions
-is_installed code
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v code)" ]; then
     code --update-extensions
 fi
 
 # update omz
-is_installed omz
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v omz)" ]; then
     omz update
 fi
 
 # update atuin-update
-is_installed atuin-update
-if [ "true" = "$INSTALLED" ]
-then
+if [ -x "$(command -v atuin-update)" ]; then
     atuin-update
 fi
