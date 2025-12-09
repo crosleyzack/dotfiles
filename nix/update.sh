c653678c67e14b71980b06fff1bc8047f3a3c3cb
crosleyzack <crosleyzack@gmail.com>
2025-12-09 16:42:50 -0500

    request user permission before update

--- nix/update.sh
+++ nix/update.sh
@@ -3,6 +3,17 @@
 FILE_PATH=$(realpath $BASH_SOURCE)
 DIR_PATH=$(dirname $FILE_PATH)
 
+echo "Make sure the home directory is correct and vscode is disabled if on Ubuntu"
+read -p "Do you want to continue? (y/n) " yn
+
+case $yn in
+    y ) echo ok, we will proceed;;
+    n ) echo exiting...;
+        exit;;
+    * ) echo invalid response;
+        exit 1;;
+esac
+
 rm -f $HOME/.config/home-manager/home.nix
 rm -rf $HOME/.config/home-manager/home
 cp $DIR_PATH/home.nix $HOME/.config/home-manager/home.nix
