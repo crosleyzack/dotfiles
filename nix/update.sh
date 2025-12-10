0b98d60af5a10c31082a0cb472d6da7e1c357d14
crosleyzack <crosleyzack@gmail.com>
2025-12-10 08:41:23 -0500

    nix: update xplr

--- nix/update.sh
+++ nix/update.sh
@@ -18,5 +18,7 @@ rm -f $HOME/.config/home-manager/home.nix
 rm -rf $HOME/.config/home-manager/home
 cp $DIR_PATH/home.nix $HOME/.config/home-manager/home.nix
 cp -r $DIR_PATH/home $HOME/.config/home-manager/home
+# this file tends to cause problems, remove it
+rm $HOME/.config/atuin/config.toml
 
 home-manager switch
