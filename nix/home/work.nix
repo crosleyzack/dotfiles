4aace6b9db5c5c01e9dcb9d16d658911f540b07d
crosleyzack <crosleyzack@gmail.com>
2025-12-11 10:31:21 -0500

    nix custom package install
    Signed-off-by: crosleyzack <crosleyzack@gmail.com>

--- nix/home/work.nix
+++ nix/home/work.nix
@@ -5,10 +5,12 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/f62d6734af4581af614cab0f2aa16bcecfc33c11.tar.gz";
     sha256 = "sha256:10w5c44wyjf29kb2r5pgqy8bahaq46a9lba2wybipkqk293zkdpr";
   });
+  myProtoc = protocPkgs.protobuf;
   protocGenGoPkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/2c36ece932b8c0040893990da00034e46c33e3e7.tar.gz";
     sha256 = "sha256:10w5c44wyjf29kb2r5pgqy8bahaq46a9lba2wybipkqk293zkdpr";
   });
+  myProtocGenGo = protocGenGoPkgs.protoc-gen-go;
 in
 {
   home.packages = with pkgs; [
@@ -30,11 +32,13 @@ in
     docker-credential-gcr
     # TODO install from protocPkgs
     # protobuf
+    # myProtoc
     buf
     grpcurl
     grpc-gateway
     # TODO install from protocGenGoPkgs
     # protoc-gen-go
+    # myProtocGenGo
     protoc-gen-go-grpc
     # apks
     apko
