04af064f9311d55d326f6ece18984400a1264dab
crosleyzack <crosleyzack@gmail.com>
2025-12-12 16:25:09 -0500

    instll more go libs

--- golang/install.sh
+++ golang/install.sh
@@ -6,7 +6,26 @@ then
     exit 1
 fi
 
+# install protobuf compiler v29.3
+FNAME="protoc-29.3-linux-x86_64.zip"
+DIR="$HOME/temp/protoc"
+
+mkdir -p $DIR
+rm -rf $DIR/*
+rm -rf $DIR/$FNAME
+rm -f $HOME/.local/bin/protoc
+
+# install protobuf compiler v29.3
+wget https://github.com/protocolbuffers/protobuf/releases/download/v29.3/protoc-29.3-linux-x86_64.zip -P $DIR
+unzip -d $DIR $DIR/$FNAME
+cp $DIR/bin/protoc $HOME/.local/bin
+rm -rf $DIR
+
 # install packages only available via go install
 go install \
     github.com/crosleyzack/xplr@latest \
+    google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.2 \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1 \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.22.0 \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.22.0 \
     github.com/protoc-gen/protoc-gen-openapiv3@latest
