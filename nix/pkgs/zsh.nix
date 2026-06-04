ac61b2f975bc3992a41cb1c6546f8ea773d98672
crosleyzack <mail@crosleyzack.com>
2026-06-04 08:55:48 -0400

    setup pyenv in zsh

--- nix/pkgs/zsh.nix
+++ nix/pkgs/zsh.nix
@@ -26,6 +26,14 @@
             export USE_GKE_GCLOUD_AUTH_PLUGIN=true
             export OUTSIDE_DOCKER_HOST="172.17.0.1"
             export HISTIGNORE="*token*:*password*:*secret*:*api*key*"
+            # for pyenv to work on nix
+            PYENV_ROOT="$HOME/.pyenv";
+            CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
+            CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include";
+            CFLAGS="-I${pkgs.openssl.dev}/include";
+            LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib";
+            CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
+            PYENV_VIRTUALENV_DISABLE_PROMPT="1";
           '';
           setOptions = [
             "NO_BEEP"
@@ -42,6 +50,7 @@
             source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
             source $HOME/.nix-profile/etc/profile.d/nix.sh
             test -f $HOME/.env && source $HOME/.env 
+            eval "$(pyenv init - zsh)"
           '';
       };
   };
