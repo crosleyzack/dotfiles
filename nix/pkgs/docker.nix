4bbe3ffbc2e299c965e1501759cf4701d568e07a
Zackary Crosley <zackary.crosley@chainguard.dev>
2026-08-20 21:49:18 -0400

    remove erroneous rootless docker var
    Signed-off-by: Zackary Crosley <zackary.crosley@chainguard.dev>

--- nix/pkgs/docker.nix
+++ nix/pkgs/docker.nix
@@ -5,7 +5,8 @@
   # (installed alongside docker-ce-rootless-extras via apt). Rootless setup
   # is bootstrapped once with `dockerd-rootless-setuptool.sh install`.
   home.sessionVariables = {
-    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/docker.sock";
+    # only uncomment this once rootless docker is setup
+    # DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/docker.sock";
   };
   home.shellAliases = {
     stop_containers = "docker stop $(docker ps -aq)";
