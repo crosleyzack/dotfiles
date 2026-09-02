86a7f8d890b286cf37b7e017882fa3e802c095cc
Zackary Crosley <zackary.crosley@chainguard.dev>
2026-09-02 10:09:27 -0400

    add work config
    Signed-off-by: Zackary Crosley <zackary.crosley@chainguard.dev>

--- nix/pkgs/cg.nix
+++ nix/pkgs/cg.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -12,4 +12,15 @@
     apko
     melange
   ];
+
+  # A new VM gets a new root disk, thus it loses /nix. The tool reads this file
+  # at every start, and copies the script to the new machine as boot metadata.
+  # The script binds /nix to the data disk again. See ../utils/bind-store.sh.
+  home.file.work_conf = {
+    target = ".config/cgw/config.toml";
+    text = ''
+      [pets]
+      script = "${config.home.homeDirectory}/dev/dotfiles/nix/utils/bind-store.sh"
+    '';
+  };
 }
