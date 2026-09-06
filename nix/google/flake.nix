83ed055f012932ee7af606fde345c04529d0e01e
crosleyzack <mail@crosleyzack.com>
2026-09-05 20:37:43 -0400

    add nix bump action

--- nix/google/flake.nix
+++ nix/google/flake.nix
@@ -2,9 +2,9 @@
     description = "Google VM Home Manager Flake";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
         home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
+            url = "github:nix-community/home-manager/release-25.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
