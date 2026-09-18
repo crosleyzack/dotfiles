21c49030f3a393f19a9277ef6b207e74dd263445
Zackary Crosley <zackary.crosley@chainguard.dev>
2026-09-18 16:00:29 -0400

    one more keybinding
    Signed-off-by: Zackary Crosley <zackary.crosley@chainguard.dev>

--- nix/pkgs/starship.nix
+++ nix/pkgs/starship.nix
@@ -7,6 +7,9 @@
           enableZshIntegration = true;
           settings = {
               format = "$all";
+              # 30ms default trips on cold inode metadata, silently dropping
+              # language modules because the file scan is left incomplete.
+              scan_timeout = 100;
               battery.disabled = true;
           };
       };
